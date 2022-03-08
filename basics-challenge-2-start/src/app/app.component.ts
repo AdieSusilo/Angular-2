20814add2b3a6ec621183897a7f00d7c2add0030
AdieSusilo <100736643+AdieSusilo@users.noreply.github.com>
2022-03-08 18:07:58 +0700

    Add files via upload

---
+++ basics-challenge-2-start/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { disableDebugTools } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  value = ""
+  isDisabled = true
+
+  reset(){
+    this.value = ""
+  }
+}
+
